732d420a8a31e641d85dd01500e7d63b9d1878c9
Hakan Gülmek <gulmekhakan6@gmail.com>
2024-05-22 00:57:33 +0300

    button and input added

--- Netflix/src/Components/view/Login.tsx
+++ Netflix/src/Components/view/Login.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { TbArrowBigRightLinesFilled } from "react-icons/tb";
 
 function Login() {
   return (
@@ -18,6 +19,20 @@ function Login() {
               İzlemeye hazır mısınız? Üye olmak ya da hesabınıza tekrar ulaşmak
               için tek yapmanız gereken e-posta adresinizi girmek.
             </span>
+          </div>
+          <div className="w-full relative">
+            <div className="flex mt-4 relative w-full">
+              <input className="h-14 pl-4 rounded border-gray-950 w-full max-w-xl ml-40 peer" />
+              <span className="absolute top-0 left-0 h-full flex items-center ml-6 pl-[152px] peer-focus:h-4 text-opacity-40 transition-all">
+                E-posta adresi
+              </span>
+              <button className="flex bg-logo-color ml-3 w-44 rounded font-extrabold text-white text-xl relative p-4 max-h-14 items-center content-center">
+                Başlayın
+                <div className="flex relative right h-6 ml-4 mt-1 items-center content-center">
+                  <TbArrowBigRightLinesFilled size={30} />
+                </div>
+              </button>
+            </div>
           </div>
         </div>
       </div>
