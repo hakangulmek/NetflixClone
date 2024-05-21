f098487436a4ac4a0823eca17e2a72c647fa1314
Hakan Gülmek <gulmekhakan6@gmail.com>
2024-05-21 18:09:06 +0300

    navbar eklendi

--- Netflix/src/Components/Navbar.tsx
+++ Netflix/src/Components/Navbar.tsx
@@ -1,26 +1,42 @@
 import React from "react";
+import ReactFlagsSelect from "react-flags-select";
+import { useState } from "react";
 
 function Navbar() {
+  const [selected, setSelected] = useState("ES");
+  const languages = {
+    US: "ENGLISH",
+    TR: "TURKISH",
+    FR: "FRANCH",
+    IT: "ITALY",
+    ES: "SPANISH",
+  };
+
   return (
-    <div className="pt-8 pl-60 absolute">
-      <div className="w-full rounded-sm h-auto flex justify-between ">
-        <div className="">
-          <span>
-            <h1 className="text-logo-color font-bold text-5xl fill-current w-[86px] h-6">
-              NETFLİX
-            </h1>
+    <div className="">
+      <div className="absolute w-full px-52 pt-8 bg-gradient-to-t from-slate-700">
+        <div className="flex justify-between w-full h-[50px] pr-6 ">
+          <span className="text-logo-color font-bold text-3xl fill-current h-auto">
+            NETFLİX
           </span>
-        </div>
-        <div>
-          <span>
-            <div></div>
-          </span>
-          <div>
-            <button>
-              <a href="#"></a>
-            </button>
+          <div className="flex gap-x-2 h-6">
+            <div className="w-full h-6 flex">
+              <ReactFlagsSelect
+                className="flag-select"
+                countries={Object.keys(languages)}
+                customLabels={languages}
+                selected={selected}
+                onSelect={(code) => setSelected(code)}
+              />
+            </div>
+            <div className="rounded-md bg-logo-color w-full">
+              <a className="text-xs font-serif p-4 text-white" href="#">
+                Oturum Aç
+              </a>
+            </div>
           </div>
         </div>
+        <div></div>
       </div>
     </div>
   );
